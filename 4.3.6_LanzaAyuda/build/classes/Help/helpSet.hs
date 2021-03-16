5c9283ce392d14bd8eef89f59dffc879bf6238a6
garciaveravictor <71645624+garciaveravictor@users.noreply.github.com>
2021-03-16 11:06:46 +0100

    organizacion

---
+++ 4.3.6_LanzaAyuda/build/classes/Help/helpSet.hs
@@ -0,0 +1,21 @@
+<?xml version='1.0' encoding='ISO−8859−1'?>
+<!DOCTYPE html>
+<helpset version="2.0">
+    <title>Java Development Environment − Help</title>
+    <maps>
+        <homeID>top</homeID>
+        <mapref location="map.jhm" />
+    </maps>
+    <view xml:lang="en" mergetype="javax.help.UniteAppendMerge">
+        <name>TOC</name>
+        <label>Tabla de contenidos</label>
+        <type>javax.help.TOCView</type>
+        <data>TOC.xml</data>
+    </view>
+    <view xml:lang="en" mergetype="javax.help.SortMerge">
+        <name>Index</name>
+        <label>Index</label>
+        <type>javax.help.IndexView</type>
+        <data>IdeHelpIndex.xml</data>
+    </view>
+</helpset>
